b9ddc2113af118d853f713604d893a168badc568
kapich1192 <kapich1192@yandex.ru>
2022-07-12 18:06:32 +0300

    developing

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,6 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_of_life.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dmitriy <kapich1192@yandex.ru>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 17:59:25 by Dmitriy           #+#    #+#             */
+/*   Updated: 2022/07/12 18:05:49 by Dmitriy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+#include <stdlib.h>
+
+#define HEIGTH 25
+#define WEIGTH 80
+#define MARKER '*'
+
+void printGameField(char **gameField);
 
 int main(void) {
-	printf("Hello World!\n");
+	
 	return (0);
 }
+
+void printGameField(char **gameField) {
+	for (int i = 0; i < HEIGTH; i++) {
+		for (int j = 0; j < WEIGTH; j++) {
+			printf("%c", gameField[i][j]);
+		}
+		printf("\n");
+	}
+}
+
